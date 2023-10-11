9cd1f26b1804b35312631ad6776867a6936094b4
Prakash Regmi <prakashregmi71@gmail.com>
2023-10-11 18:35:46 +1100

    login page using react with basic css

---
+++ webpage/src/login.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import './form.css';
+import { Link } from 'react-router-dom'
+
+function Login() {
+    const [loginValues, setLoginValues] = useState({
+        username:'',
+        password:''
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setLoginValues(prev => ({ ...loginValues, [name]: value }))
+    }
+    return (
+        <div class = 'main-page'> 
+            <form class = 'main-form' action='' onSubmit={handleSubmit}>
+            <h2> Sign In </h2>
+                <div>
+                    <label class = 'label' htmlFor='email'>Username</label>
+                    <input class = 'input' name = 'email' type = 'email' placeholder = 'Please Enter your email'
+                    onChange={handleChange}         
+                    />
+                </div>
+                <div>
+                    <label  class = 'label' htmlFor='password'>Password</label>
+                    <input  class = 'input' name = 'password' type = 'password' placeholder = 'Please Enter your Password'
+                    onChange={handleChange}
+                    />
+                </div>
+                <button class = 'button'type = 'submit'>Login</button>
+                <Link to='/signup'>Create Account</Link>
+            </form>    
+         </div>
+    )
+}
+export default Login 
